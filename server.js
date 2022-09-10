0d8cbd0e66c384b3a7fad98f4c6a10eacc1c30a4
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-09-10 12:02:14 +0800

    improved server yes

--- server.js
+++ server.js
@@ -1,28 +1,47 @@
-// Importing the required modules
-const WebSocketServer = require('ws');
- 
-// Creating a new websocket server
-const wss = new WebSocketServer.Server({ port: process.env.PORT || 8080 })
- 
-// Creating connection using websocket
-wss.on("connection", ws => {
-    console.log("new client connected");
-    // sending message
-    ws.on("message", data => {
-        console.log(`Client has sent us: ${data}`)
-        wss.clients.forEach(function each(client) {
-            client.send(`${data}`);
-        });
-    });
-    // handling what to do when clients disconnects from server
-    ws.on("close", () => {
-        console.log("the client has connected");
-    });
-    // handling client connection error
-    ws.onerror = function () {
-        console.log("Some Error occurred")
-    }
-});
-console.log("The WebSocket server is running on port 8080");
+const WebSocketServer = require("websocket").server;
+const http = require("http");
 
-// client.send(`${message}`);
+const port = process.env.PORT || 8080
+
+const server = http.createServer((req, res) => {
+	res.writeHead(400)
+	res.end()
+});
+server.listen(8080)
+
+const wss = new WebSocketServer({
+	httpServer: server,
+	autoAcceptConnections: false,
+})
+
+function isRepliclientOrigin(origin) {
+	// TODO: figure out how to do this
+	return true;
+};
+
+wss.on("request", (req) => {
+	if (!isRepliclientOrigin(req.origin)) {
+		req.reject();
+		return;
+	}
+
+	let connection = req.accept(undefined, req.origin);
+
+	console.log("Peer '" + connection.socket.remoteAdress + "' has connected.")
+	connection.on("message", (message) => {
+		if (message.type !== "utf8") {
+			return;
+		};
+		wss.connections.forEach((_connection) => {
+            // checks if the connection object is same as the connection object by the client
+			if (_connection === connection) {
+				return;
+			};
+			_connection.sendUTF(message.utf8Data);
+		});
+	});
+
+	connection.on("close", (reasonCode, desc) => {
+		console.log("Peer '" + connection.socket.remoteAdress + "' disconnected.")
+	});
+});
